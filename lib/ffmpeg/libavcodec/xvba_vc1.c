51b461f45da7b85c0b55215f245765a0066547a1
fritsch <peter.fruehberger@gmail.com>
2011-11-04 17:38:07 +0100

    ffmpeg: xvba implement VC1 decoder - revisit avc_intra_flag (got from pict_type) - change profile

--- lib/ffmpeg/libavcodec/xvba_vc1.c
+++ lib/ffmpeg/libavcodec/xvba_vc1.c
@@ -20,27 +20,115 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "xvba.h"
+#include "xvba_internal.h"
 #include "vc1.h"
 #include "vc1data.h"
+#include <assert.h>
 
-static int xvba_vc1_start_frame(AVCodecContext *avctx, av_unused const uint8_t *buffer, av_unused uint32_t size)
+
+/** @file
+ * Implement structures of ffmpeg <-> XvBA
+ */
+
+/* Initialize and start decoding a frame with XvBA */
+static int xvba_vc1_start_frame(AVCodecContext *avctx, 
+				av_unused const uint8_t *buffer, 
+				av_unused uint32_t size)
 {
     VC1Context * const v = avctx->priv_data;
     MpegEncContext * const s = &v->s;
+    struct xvba_context *hwaccel_context;
+    
+    hwaccel_context = (struct xvba_context *)avctx->hwaccel_context;
+    assert(hwaccel_context);
+    
+    hwaccel_context->num_slices = 0;
+    hwaccel_context->data_buffer->data_size_in_buffer = 0;
+    
     return 0;
 }
 
+/* End a hardware decoding based frame */
 static int xvba_vc1_end_frame(AVCodecContext *avctx)
 {
-    VC1Context * const v = avctx->priv_data;
+    VC1Context* const v = avctx->priv_data;
+    MpegEncContext* const s = &v->s;
+    struct xvba_context *hwaccel_context;
+    XVBAPictureDescriptor *pic_descriptor;
+    
+    hwaccel_context = (struct xvba_context *)avctx->hwaccel_context;
+    assert(hwaccel_context);
 
+    pic_descriptor = hwaccel_context->picture_descriptor_buffer->bufferXVBA;
+    
+    av_dlog(avctx, "xvba_vc1_end_frame()\n");
+    
+    /* Fill in Parameters - for reference see AMD sdk documentation */
+    pic_descriptor->profile                                 = ff_xvba_translate_profile(avctx->profile);
+    pic_descriptor->level                                   = avctx->level;
+    pic_descriptor->width_in_mb                             = s->mb_width;
+    pic_descriptor->height_in_mb                            = s->mb_height;
+    pic_descriptor->picture_structure                       = s->picture_structure;
+    // xvba-video set this to 1 only 4:2:0 supported
+    // doc says: if not set, choose 1 - we try this
+    pic_descriptor->chroma_format                           = s->chroma_format ? s->chroma_format : 1;
+    pic_descriptor->avc_intra_flag                          = (s->pict_type == FF_I_TYPE) ? 1 : 0;
+    pic_descriptor->avc_reference                           = (s->current_picture_ptr->reference & 3) ? 1 : 0;
+    
+    // VC-1 explicit parameters see page 30 of sdk
+    // sps_info
+    pic_descriptor->sps_info.vc1.postprocflag               = v->postprocflag;
+    
+    // set pull down to true, if one of these three is present
+    pic_descriptor->sps_info.vc1.pulldown                   = v->rptfrm | v->tff | v->rff;
+    pic_descriptor->sps_info.vc1.interlace                  = v->interlace;
+    pic_descriptor->sps_info.vc1.tfcntrflag                 = v->tfcntrflag;
+    pic_descriptor->sps_info.vc1.finterpflag                = v->finterpflag;
+    pic_descriptor->sps_info.vc1.reserved                   = 1;
+    // eventually check if this makes sense together with interlace
+    pic_descriptor->sps_info.vc1.psf                        = v->psf;
+    // what about if it is a frame (page 31)
+    // looked at xvba-driver
+    pic_descriptor->sps_info.vc1.second_field               = !s->first_field;
+    pic_descriptor->sps_info.vc1.xvba_vc1_sps_reserved      = 0;
+    
+    // VC-1 explicit parameters see page 30 of sdk
+    // pps_info
+    pic_descriptor->pps_info.vc1.panscan_flag               = v->panscanflag;
+    pic_descriptor->pps_info.vc1.refdist_flag               = v->refdist_flag;
+    pic_descriptor->pps_info.vc1.loopfilter                 = s->loop_filter;
+    pic_descriptor->pps_info.vc1.fastuvmc                   = v->fastuvmc;
+    pic_descriptor->pps_info.vc1.extended_mv                = v->extended_mv;
+    pic_descriptor->pps_info.vc1.dquant                     = v->dquant;
+    pic_descriptor->pps_info.vc1.vstransform                = v->vstransform;
+    pic_descriptor->pps_info.vc1.overlap                    = v->overlap;
+    pic_descriptor->pps_info.vc1.quantizer                  = v->quantizer_mode;
+    pic_descriptor->pps_info.vc1.extended_dmv               = v->extended_dmv;   
+    pic_descriptor->pps_info.vc1.maxbframes                 = s->max_b_frames;
+    pic_descriptor->pps_info.vc1.rangered                   = (pic_descriptor->profile == PROFILE_SIMPLE) ? 0 : v->rangered;   
+    pic_descriptor->pps_info.vc1.syncmarker                 = (pic_descriptor->profile == PROFILE_SIMPLE) ? 0 : s->resync_marker;
+    pic_descriptor->pps_info.vc1.multires                   = v->multires;
+    pic_descriptor->pps_info.vc1.reserved                   = 1;
+    pic_descriptor->pps_info.vc1.range_mapy_flag            = v->range_mapy_flag;
+    pic_descriptor->pps_info.vc1.range_mapy                 = v->range_mapy;
+    pic_descriptor->pps_info.vc1.range_mapuv_flag           = v->range_mapuv_flag;
+    pic_descriptor->pps_info.vc1.range_mapuv                = v->range_mapuv;
+    pic_descriptor->pps_info.vc1.xvba_vc1_pps_reserved      = 0;
+    
+    ff_draw_horiz_band(s, 0, s->avctx->height);
+  
     return 0;
 }
 
 static int xvba_vc1_decode_slice(AVCodecContext *avctx, const uint8_t *buffer, uint32_t size)
 {
-    VC1Context * const v = avctx->priv_data;
-    MpegEncContext * const s = &v->s;
+    struct xvba_context *hwaccel_context;
+    
+    hwaccel_context = (struct xvba_context *)avctx->hwaccel_context;
+    assert(hwaccel_context);
+    
+    ff_xvba_add_slice_data(hwaccel_context, buffer, size);
     return 0;
 }
 
